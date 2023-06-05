78259c62935c3c65987bece77b597db47572c82b
nguyen34 <nguyenjohnson65@gmail.com>
2023-06-04 22:55:11 -0700

    Added boostrap. basic components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
-  return <div><Message /></div>
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        <span>Hello World</span>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
